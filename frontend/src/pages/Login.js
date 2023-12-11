346425290e51e4f23de5bdb7248d357f32994cf7
ALAK007 <lakhdarayoub515@gmail.com>
2023-12-11 16:18:33 +0100

    some changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,9 +4,26 @@ import BreadCrumb from "../components/BreadCrumb";
 import { Link } from 'react-router-dom';
 import Container from '../components/Container';
 import CustomInput from '../components/CustomInput';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
+const loginSchema = yup.object({
+  email: yup.string().email("Email Should be valid")
+  .required("Email Address is Required"),
+  password: yup.string().required("Password is Required"),
+});
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema : loginSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+    },
+  });
   return (
     <>
       <Meta title={"Login"} />
@@ -16,9 +33,29 @@ const Login = () => {
             <div className="col-12">
                 <div className="auth-card">
                     <h3 className='text-center mb-3'>Login</h3>
-                    <form action="" className='d-flex flex-column gap-15'>
-                        <CustomInput type="email" name='email' placeholder='Email'/>
-                        <CustomInput type="password" name='password' placeholder='Password'/>
+                    <form action="" onSubmit={formik.handleSubmit} className='d-flex flex-column gap-15'>
+                        <CustomInput 
+                        type="email" 
+                        name='email' 
+                        placeholder='Email'
+                        onChange={() => formik.handleChange("email")}
+                        onBlur={() => formik.handleBlur("email")}
+                        value={formik.values.email}
+                        />
+                        <div className="error">
+                          {formik.touched.email && formik.errors.email}
+                        </div>
+                        <CustomInput 
+                        type="password" 
+                        name='password' 
+                        placeholder='Password'
+                        onChange={() => formik.handleChange("password")}
+                        onBlur={() => formik.handleBlur("password")}
+                        value={formik.values.password}
+                        />
+                        <div className="error">
+                          {formik.touched.password && formik.errors.password}
+                        </div>
                         <div>
                             <Link to='/forgot-password'>Forgot Password</Link>
                             <div className='d-flex justify-content-center gap-15 align-items-center mt-4'>
