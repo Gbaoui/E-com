675ec92a8b4e984aca6873a7f67ff67002d5db5e
Abdellah Gbaoui <gbaoui.abdellah@gmail.com>
2023-12-11 16:20:44 +0100

    linked brand

--- admin-app/src/app/store.js
+++ admin-app/src/app/store.js
@@ -6,9 +6,10 @@ import brandReducer from "../features/brand/brandSlice";
 import pCategoryReducer from "../features/pcategory/pcategorySlice";
 import colorReducer from "../features/color/colorSlice";
 import enquiryReducer from "../features/enquiry/enquirySlice";
+import uploadReducer from "../features/upload/uploadSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer, customer:customerReducer ,product:productReducer, brand:brandReducer, pCategory:pCategoryReducer, color:colorReducer, enquiry:enquiryReducer },
+  reducer: { auth: authReducer, customer:customerReducer ,product:productReducer, brand:brandReducer, pCategory:pCategoryReducer, color:colorReducer, enquiry:enquiryReducer, upload:uploadReducer },
 });
 
 export default store;
--- admin-app/src/features/pcategory/pcategoryService.js
+++ admin-app/src/features/pcategory/pcategoryService.js
@@ -1,20 +1,45 @@
-import axios from 'axios'
-import {base_url} from '../../utils/base_url'
-import { FaEdit } from 'react-icons/fa';
-import { MdDelete } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { base_url } from "../../utils/base_url";
+import { config } from "../../utils/axiosconfig";
 
+const getProductCategories = async () => {
+  const response = await axios.get(`${base_url}category/`);
 
+  return response.data;
+};
+const createCategory = async (category) => {
+  const response = await axios.post(`${base_url}category/`, category, config);
 
-const getProductCategories = async() =>{
-    const response = await axios.get(`${base_url}category/`);
-    return response.data
+  return response.data;
 };
 
+const getProductCategory = async (id) => {
+  const response = await axios.get(`${base_url}category/${id}`, config);
 
+  return response.data;
+};
+
+const deleteProductCategory = async (id) => {
+  const response = await axios.delete(`${base_url}category/${id}`, config);
+
+  return response.data;
+};
+const updateProductCategory = async (category) => {
+  console.log(category);
+  const response = await axios.put(
+    `${base_url}category/${category.id}`,
+    { title: category.pCatData.title },
+    config
+  );
+
+  return response.data;
+};
 const pCategoryService = {
-    getProductCategories,
+  getProductCategories,
+  createCategory,
+  getProductCategory,
+  deleteProductCategory,
+  updateProductCategory,
 };
-
 
 export default pCategoryService;
