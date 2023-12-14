b90915a72a58770c8a426a7eec01278d180a6524
ALAK007 <lakhdarayoub515@gmail.com>
2023-12-14 03:33:14 +0100

    last 1

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,36 +1,29 @@
-import React from 'react'
+import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from './Header';
-import Footer from './Footer';
-import { ToastContainer } from 'react-toastify';
+import Footer from "./Footer";
+import Header from "./Header";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
-
-
 const Layout = () => {
   return (
     <>
-    <Header />
-    <Outlet />
-    <Footer />
-    <ToastContainer 
-    position="top-right"
-    autoClose={5000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    theme="light"/>
-<<<<<<< HEAD
-=======
-     {/* <button onClick={showToast}>Show Test Toast</button> */}
->>>>>>> 5b5128047077b3fe01af36ef0d5ab95b82ce6046
+      <Header />
+      <Outlet />
+      <Footer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
